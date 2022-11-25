3ac6b54b2a4d07636174db6986a5ebf06667dee8
Manjunatha reddy Lingamdinne <88681651+manjunathsse@users.noreply.github.com>
2022-11-25 17:22:44 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class demo {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String str=s.next();
+		str=str.toLowerCase();
+		boolean mark[]=new boolean[26];
+		boolean b=false;
+		int index=0;
+		for(int i=0;i<str.length();i++) {
+			if('a'<=str.charAt(i) && 'z'>=str.charAt(i)) {
+				index=str.charAt(i)-'a';
+			}
+			mark[index]=true;
+			
+		}
+		for(int i=0;i<26;i++) {
+			if(mark[i]==true) {
+				b=true;
+			}
+			else {
+				b=false;
+				break;
+			}
+		}
+		if(b==true) {
+			System.out.println("Pangram string");
+		}
+		else {
+			System.out.println("not a pangram string");
+		}
+	}
+
+}
