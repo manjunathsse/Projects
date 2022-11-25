ace71857fa0f437c73d46b8c428c4f68ca896af9
Manjunatha reddy Lingamdinne <88681651+manjunathsse@users.noreply.github.com>
2022-11-25 17:24:17 +0530

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,88 +1,50 @@
-package com.cgpa.Connector;
-import java.util.Scanner;
+import java.util.*;
 
-class Solution {
+enum Month{
+	JAN,FEB,MAR,APR,MAY,JUNE,JULY,AUG,SEPT,OCT,NOV,DEC
+}
+
+enum Crypto{
+	BTC,DOGE,ETH,XRP,TRX;
+	//to create parameterized constructor we should create BTC();
+	private int x=0;
+	Crypto(){
+		//System.out.println("CRYPTO is created");
+	}
+	public int getRupees() {
+		return x;
+
+	}
+	public void setRupees(int x) {
+		this.x=x;
+	}
+}
+
+public class Solution {
 
 	public static void main(String[] args) {
-		Scanner s=new Scanner(System.in);
-		System.out.println("Please enter your name");
-		String name=s.next();
-		try {
-			System.out.println("Enter number of subjects passed?");
-			int subjects=s.nextInt();
-			System.out.println("Hii"+" "+name+" "+"is every subject you completed is 4 credit subject(Y/N)");
-			String option=s.next();
-			if(option.equals("y") || option.equals("Y")) {
-				CGPA c=new CGPA();
-				System.out.println(c.cgpa(subjects));
-			}
-			else if(option.equals("N") || option.equals("n")){
-				CGPA1 c=new CGPA1();
-				System.out.println(c.cgpa1(subjects));
-			}
-		}
-		catch(Exception e) {
-			System.out.println("Please enter valid input");
+		/*Month x=Month.APR;
+		System.out.println(x);
+		Month[] values = Month.values();// stores the enum values in array
+		for(Month month:values) {
+			System.out.println(month);
 		}
 		
-
-	}
-}
-	
-class Grade{	
-	public static int grade() {
-		Scanner s=new Scanner(System.in);
-		String ch=s.next();
-		int gradePoint=0;
-		if(ch.equals("s") || ch.equals("S")) {
-			gradePoint=10;
-		}
-		else if(ch.equals("a") || ch.equals("A")) {
-			gradePoint=9;
-		}
-		else if(ch.equals("b") || ch.equals("B")) {
-			gradePoint=8;
-		}
-		else if(ch.equals("c") || ch.equals("C")) {
-			gradePoint=7;
-		}
-		else if(ch.equals("d") || ch.equals("D")) {
-			gradePoint=6;
-		}		
-		else if(ch.equals("e") || ch.equals("E")) {
-			gradePoint=5;
-		}
-		return gradePoint;
+		System.out.println(x.ordinal());// gives the index of the month in enum
+		*/
+		Crypto x=Crypto.BTC;
+		Crypto y=Crypto.DOGE;
+		y.setRupees(100);
+		System.out.println(y.getRupees());
 	}
 }
 
-
-class CGPA{
-		
-	public static double cgpa(int subjects) {
-		int credits=subjects*4;
-		Grade g=new Grade();
-		int gradePoits=0;
-		System.out.println("Please enter the grade");
-		for(int i=0;i<subjects;i++) {
-			gradePoits+=g.grade()*4;
-		}
-		return (double)gradePoits/credits;
-	}
-}
-
-class CGPA1{
-	
-	public static double cgpa1(int subjects) {
-		Scanner s=new Scanner(System.in);
-		System.out.println("Please enter respective grade and credits");
-		Grade g=new Grade();
-		int credits=0;
-		int gradePoits=0;
-		for(int i=0;i<subjects;i++) {
-			gradePoits+=g.grade()*credits;
-			credits+=s.nextInt();
-		}
-		return (double)gradePoits/credits;
-	}
-}
+/* properties of the enum:
+ * when enum is created a class is created with the given name and its extends enum and the class becomes final
+ * the variables in the enum with objects of the class.
+ * 1. Enum can implement one or more interface
+ * 2. It cant extends another class
+ * 3. Enums are absract class
+ * 4. All variables defined in enum are public static and final
+ * 5. toString() meathod is overridden in enums.
+ */
